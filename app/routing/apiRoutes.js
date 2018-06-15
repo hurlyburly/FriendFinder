bcbea274c0273cf86096326eaa24505592995413
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-06-14 20:45:32 -0500

    adding in more routing code to routing folder files and adding in basic html in public folder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,9 @@
+var friendData = require("./data/friends");
+
+module.exports = function (app) {
 // Displays all possible friends
 app.get("/api/friends", function(req, res) {
-    return res.json(req.params.friends);
+    return res.json(friendData);
   });
   
 // Create New Friends Data - takes in JSON input
@@ -8,15 +11,11 @@ app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newFriend = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-  
+
     console.log(newFriend);
   
-    characters.push(newFriend);
+    friendData.push(newFriend);
   
-    res.json(newFriend);
+    res.json(true);
   });
-  
+};
